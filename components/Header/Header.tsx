8a852c1841560d76b0e9a0828f23c2ac650b1e02
Kumar Sanu <kumsanu@WKMZTE07F168.global.publicisgroupe.net>
2023-02-03 08:46:24 +0530

    added landing page

---
+++ components/Header/Header.tsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "20px",
+        fontSize: "24px",
+        background: "white",
+      }}
+    >
+      <div>
+        {/* <img>Logo</img> */}
+        <div>Tools</div>
+      </div>
+      <div>User</div>
+    </div>
+  );
+};
+
+export default Header;
